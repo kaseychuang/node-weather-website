dfb349c38cb174f8a10a5415729c0f0699594c1e
Kasey Chuang <chuangky@uci.edu>
2020-07-12 00:16:49 -0700

    set up app for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 const publicDir = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
 const partialsPath = path.join(__dirname, '../templates/partials');
@@ -100,6 +102,6 @@ app.get('*', (req, res) => {
 })
 
 // start the server
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is up on port 3000.')
 });
