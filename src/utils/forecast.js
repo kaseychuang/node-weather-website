2397842b94be8fd54ffc4ed034514f5dbf4ad46f
Kasey Chuang <chuangky@uci.edu>
2020-07-11 19:09:09 -0700

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require("request")
+
+const forecast = (latitude, longitude, callback) => {
+    // make api call
+    const url = "http://api.weatherstack.com/current?access_key=20d8273bba5448b8a0937050b5200638&query=" + latitude + "," + longitude;
+    request({url, json: true}, (error, {body}) => {
+        if (error){
+            callback(error, undefined);
+        }
+        else if (body.error){
+            callback("Unable to find location", undefined);
+        }
+        else{
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out." )
+        }
+    })
+    // error handling 
+
+
+}
+
+module.exports = forecast;
